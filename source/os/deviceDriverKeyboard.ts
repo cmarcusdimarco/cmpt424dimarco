cbc74924f36f3f0c8fa4d4dff3db93e8d7b7e84b
cmarcusdimarco <C.DiMarco1@marist.edu>
2022-09-15 21:38:26 -0400

    #5: Added keyboard input for symbols and punctuation.

--- source/os/deviceDriverKeyboard.ts
+++ source/os/deviceDriverKeyboard.ts
@@ -42,11 +42,106 @@ module TSOS {
                 }
                 // TODO: Check for caps-lock and handle as shifted if so.
                 _KernelInputQueue.enqueue(chr);
-            } else if (((keyCode >= 48) && (keyCode <= 57)) ||   // digits
-                        (keyCode == 32)                     ||   // space
-                        (keyCode == 13)                     ||   // enter
-                        (keyCode == 8)) {                        // backspace
+            } else if ((keyCode == 32)                     ||   // space
+                       (keyCode == 13)                     ||   // enter
+                       (keyCode == 8)) {                        // backspace
                 chr = String.fromCharCode(keyCode);
+                _KernelInputQueue.enqueue(chr);
+            } else if (((keyCode >= 48) && (keyCode <= 57))) {  // digits
+                // Check for shifted keyboard number keys
+                if (isShifted) {
+                    switch (keyCode) {
+                        case 48:
+                            chr = ')';
+                            break;
+                        case 49:
+                            chr = '!';
+                            break;
+                        case 50:
+                            chr = '@';
+                            break;
+                        case 51:
+                            chr = '#';
+                            break;
+                        case 52:
+                            chr = '$';
+                            break;
+                        case 53:
+                            chr = '%';
+                            break;
+                        case 54:
+                            chr = '^';
+                            break;
+                        case 55:
+                            chr = '&';
+                            break;
+                        case 56:
+                            chr = '*';
+                            break;
+                        case 57:
+                            chr = '(';
+                            break;
+                        default: break;
+                    }
+                } else {
+                    chr = String.fromCharCode(keyCode);
+                }
+                _KernelInputQueue.enqueue(chr);
+            } else if ((keyCode >= 186 && keyCode <= 192)  ||   // Symbols, IE Keycodes
+                       (keyCode >= 219 && keyCode <= 222)  ||   // Symbols, IE Keycodes
+                       (keyCode >= 44 && keyCode <= 47)    ||   // Symbols, Mozilla/ASCII Keycodes
+                       (keyCode == 39)                     ||   // Symbols, Mozilla/ASCII Keycodes
+                       (keyCode == 59)                     ||   // Symbols, Mozilla/ASCII Keycodes
+                       (keyCode == 61)                     ||   // Symbols, Mozilla/ASCII Keycodes
+                       (keyCode >= 91 && keyCode <= 93)    ||   // Symbols, Mozilla/ASCII Keycodes
+                       (keyCode == 96)) {
+                switch (keyCode) {
+                    case 59:
+                    case 186:
+                        chr = isShifted ? ':' : ';';
+                        break;
+                    case 61:
+                    case 187:
+                        chr = isShifted ? '+' : '=';
+                        break;
+                    case 44:
+                    case 188:
+                        chr = isShifted ? '<' : ',';
+                        break;
+                    case 45:
+                    case 189:
+                        chr = isShifted ? '_' : '-';
+                        break;
+                    case 46:
+                    case 190:
+                        chr = isShifted ? '>' : '.';
+                        break;
+                    case 47:
+                    case 191:
+                        chr = isShifted ? '?' : '/';
+                        break;
+                    case 96:
+                    case 192:
+                        chr = isShifted ? '~' : '`';
+                        break;
+                    case 91:
+                    case 219:
+                        chr = isShifted ? '{' : '[';
+                        break;
+                    case 92:
+                    case 220:
+                        chr = isShifted ? '|' : String.fromCharCode(92);    // Using fromCharCode to avoid escaping '\'
+                        break;
+                    case 93:
+                    case 221:
+                        chr = isShifted ? '}' : ']';
+                        break;
+                    case 39:
+                    case 222:
+                        chr = isShifted ? '"' : "'";
+                        break;
+                    default: break;
+                }
                 _KernelInputQueue.enqueue(chr);
             }
         }
