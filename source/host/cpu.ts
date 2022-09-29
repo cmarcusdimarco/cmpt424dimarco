87637131efd42ac82a899a66517d4683ab59a9b3
C. Marcus DiMarco <79232853+cmarcusdimarco@users.noreply.github.com>
2022-09-29 11:33:38 -0400

    Adding CPU from CMPT422.

--- source/host/cpu.ts
+++ source/host/cpu.ts
@@ -13,30 +13,310 @@
 
 module TSOS {
 
-    export class Cpu {
+    import { Ascii } from "../libraries/Ascii";
+    import {System} from "../System";
+    import {Hardware} from "./Hardware";
+    import { ClockListener } from "./imp/ClockListener";
+    import { InterruptController } from "./InterruptController";
+    import { MMU } from "./MMU";
 
-        constructor(public PC: number = 0,
-                    public Acc: number = 0,
-                    public Xreg: number = 0,
-                    public Yreg: number = 0,
-                    public Zflag: number = 0,
-                    public isExecuting: boolean = false) {
+    export class Cpu extends Hardware implements ClockListener {
 
+        private cpuClockCount: number;
+        private IC: InterruptController;            // Pointer to System's Interrupt Controller
+        private MMU: MMU;                           // Pointer to System's MMU
+        private system: System;                     // Pointer to System
+        private currentStep: number = 0x0;          // Placeholder to identify current step of instruction cycle
+
+        // Registers
+        private accumulator: number = 0x00;
+        private instructionRegister: number = 0x00; // Register to hold current instruction
+        private programCounter: number = 0x0000;    // Register to hold program counter
+        private xRegister: number = 0x00;
+        private yRegister: number = 0x00;
+        private carryFlag: number = 0x0;
+        private zFlag: number = 0x0;
+
+
+        // Multi-dimensional array representing instruction set. First dimension stores op codes.
+        // Second dimension stores number of operands to expect after op code or stores 0xFF if the number of
+        // operands is conditional.
+        private instructionSet: number[][] = [
+            [0x00,0x6D,0x8A,0x8D,0x98,0xA0,0xA2,0xA8,0xA9,0xAA,0xAC,0xAD,0xAE,0xD0,0xEA,0xEC,0xEE,0xFF],
+            [0x00,0x02,0x00,0x02,0x00,0x01,0x01,0x00,0x01,0x00,0x02,0x02,0x02,0x01,0x00,0x02,0x02,0xFF]
+        ];
+        /**
+         *      OpCode  |   Operands    |   Description
+         *      -----------------------------------------------------------------------------------------
+         *      00      |   0           |   Break / halt
+         *      6D      |   2           |   Add with carry - result into accumulator
+         *      8A      |   0           |   Load the accumulator from the X register
+         *      8D      |   2           |   Store the accumulator in memory
+         *      98      |   0           |   Load the accumulator from the Y register
+         *      A0      |   1           |   Load the Y register with a constant
+         *      A2      |   1           |   Load the X register with a constant
+         *      A8      |   0           |   Load the Y register from the accumulator
+         *      A9      |   1           |   Load the accumulator with a constant
+         *      AA      |   0           |   Load the X register from the accumulator
+         *      AC      |   2           |   Load the Y register from memory
+         *      AD      |   2           |   Load the accumulator from memory
+         *      AE      |   2           |   Load the X register from memory
+         *      D0      |   1           |   Branch n bytes if Z flag = 0
+         *      EA      |   0           |   No operation
+         *      EC      |   2           |   Compare a byte in memory to X register, set Z flag if equal
+         *      EE      |   2           |   Increment the value of a byte
+         *      FF      |   0 or 2      |   If 1 in X register, print the integer in the Y register
+         *                                  If 2 in X register, print 0x00-terminated string starting at
+         *                                      address in Y register
+         *                                  If 3 in X register, print 0x00-terminated string starting at
+         *                                      address in operand
+         */
+
+        constructor() {
+            super('CPU', 0);
+            this.cpuClockCount = 0;
         }
 
-        public init(): void {
-            this.PC = 0;
-            this.Acc = 0;
-            this.Xreg = 0;
-            this.Yreg = 0;
-            this.Zflag = 0;
-            this.isExecuting = false;
+        // Reset clock count on boot and assign MMU pointer - to be called by startSystem()
+        public boot(MMU: MMU, IC: InterruptController) {
+            this.cpuClockCount = 0;
+            this.MMU = MMU;
+            this.IC = IC;
         }
 
-        public cycle(): void {
-            _Kernel.krnTrace('CPU cycle');
-            // TODO: Accumulate CPU usage and profiling statistics here.
-            // Do the real work here. Be sure to set this.isExecuting appropriately.
+        public pulse() {
+            this.cpuClockCount++;
+            this.log(`received clock pulse - CPU Clock Count: ${this.cpuClockCount}`);
+            this.log(`CPU State | Mode: 0 PC: ${this.hexLog(this.programCounter, 4)} ` +
+                `IR: ${this.hexLog(this.instructionRegister, 2)} ` +
+                `Acc: ${this.hexLog(this.accumulator, 2)} ` +
+                `xReg: ${this.hexLog(this.xRegister, 2)} ` +
+                `yReg: ${this.hexLog(this.yRegister, 2)} ` +
+                `zFlag: ${this.hexLog(this.zFlag, 1)} ` +
+                `Step: ${this.hexLog(this.currentStep, 1)}`);
+
+            switch (this.currentStep) {
+                case 0x0:
+                    this.fetch();
+                    break;
+                case 0x1:
+                    this.decode();
+                    break;
+                case 0x2:
+                    this.decode2();
+                    break;
+                case 0x3:
+                    this.execute();
+                    break;
+                case 0x4:
+                    this.execute2();
+                    break;
+                case 0x5:
+                    this.writeBack();
+                    break;
+                case 0x6:
+                    this.interruptCheck();
+                    break;
+                default:
+                    break;
+            }
         }
+
+        // Pipelining outline
+        private fetch() {
+            // Get the next instruction from the memory address in the program counter
+            this.instructionRegister = this.MMU.readImmediate(this.programCounter);
+            // End by incrementing program counter and step counter
+            this.programCounter++;
+            this.currentStep++;
+        }
+
+        private decode() {
+            // Interpret the instruction and determine if more operands are necessary for execution
+            let numOperands: number;
+
+            // Find instructionIndex and numOperands for the current instruction
+            for (let i = 0x00; i < this.instructionSet[0x00].length; i++) {
+                if (this.instructionSet[0x00][i] == this.instructionRegister) {
+                    numOperands = this.instructionSet[0x01][i];
+                    break;
+                }
+            }
+
+            // For conditional opcodes, check X register to determine numOperands
+            if (numOperands == 0xFF) {
+                if (this.xRegister == 0x03) {
+                    numOperands = 0x02;
+                } else {
+                    numOperands = 0x00;
+                }
+            }
+
+            // If no operands, jump to execute.
+            // If one operand, store operand where needed and skip decode2.
+            // If two operands, store low order byte.
+            switch (numOperands) {
+                case 0x00:
+                    this.currentStep += 2;
+                    return;
+                case 0x01:
+                    switch (this.instructionRegister) {
+                        // Loading constant cases, skip to interrupt check
+                        case 0xA0:
+                            this.yRegister = this.MMU.readImmediate(this.programCounter);
+                            this.currentStep = 6;
+                            break;
+                        case 0xA2:
+                            this.xRegister = this.MMU.readImmediate(this.programCounter);
+                            this.currentStep = 6;
+                            break;
+                        case 0xA9:
+                            this.accumulator = this.MMU.readImmediate(this.programCounter);
+                            this.currentStep = 6;
+                            break;
+                        // Branch to relative address
+                        case 0xD0:
+                            this.MMU.setLowOrder(this.MMU.readImmediate(this.programCounter));
+                            this.currentStep += 2;
+                            break;
+                        default:
+                            this.log(`ERR: ${this.instructionRegister} is not a valid instruction.`);
+                            break;
+                    }
+                    this.programCounter++;
+                    return;
+                case 0x02:
+                    this.MMU.setLowOrder(this.MMU.readImmediate(this.programCounter));
+                    this.programCounter++;
+                    this.currentStep++;
+                    return;
+                default:
+                    return;
+            }
+        }
+
+        private decode2() {
+            // If reached, load high order byte
+            this.MMU.setHighOrder(this.MMU.readImmediate(this.programCounter));
+            this.programCounter++;
+            this.currentStep++;
+        }
+
+        private execute() {
+            // Perform the action specified by the instruction
+            switch (this.instructionRegister) {
+                case 0x00:  // Halt
+                    this.system.stopSystem();
+                    break;
+                case 0x6D:  // Add with carry
+                    this.accumulator += this.MMU.read();
+                    if (this.accumulator > 0xFF) {
+                        this.accumulator -= 0x100;
+                        this.carryFlag = 1;
+                    }
+                    break;
+                case 0x8A:  // Load accumulator from X register
+                    this.accumulator = this.xRegister;
+                    break;
+                case 0x8D:  // Store accumulator in memory
+                    this.MMU.write(this.accumulator);
+                    break;
+                case 0x98:  // Load accumulator from Y register
+                    this.accumulator = this.yRegister;
+                    break;
+                case 0xA8:  // Load Y register from accumulator
+                    this.yRegister = this.accumulator;
+                    break;
+                case 0xAA:  // Load X register from accumulator
+                    this.xRegister = this.accumulator;
+                    break;
+                case 0xAC:  // Load Y register from memory
+                    this.yRegister = this.MMU.read();
+                    break;
+                case 0xAD:  // Load accumulator from memory
+                    this.accumulator = this.MMU.read();
+                    break;
+                case 0xAE:  // Load X register from memory
+                    this.xRegister = this.MMU.read();
+                    break;
+                case 0xD0:  // Branch n bytes if Z flag not set
+                    if (this.zFlag == 0) {
+                        if (this.MMU.getLowOrderByte() <= 0x7F) {
+                            this.programCounter += this.MMU.getLowOrderByte();
+                        } else {
+                            this.programCounter -= (256 - this.MMU.getLowOrderByte());
+                        }
+                    }
+                    break;
+                case 0xEA:  // No operation
+                    break;
+                case 0xEC:  // Compare memory to X register, set Z flag if equal
+                    if (this.xRegister == this.MMU.read()) {
+                        this.zFlag = 1;
+                    }
+                    break;
+                case 0xEE:  // Increment value of a byte
+                    this.accumulator = this.MMU.read();
+                    this.currentStep -= 2;  // Relative decrement to reach execute2()
+                    break;
+                case 0xFF:  // System call
+                    switch (this.xRegister) {
+                        case 0x1:
+                            // Print integer in Y register
+                            _StdOut.putText(this.yRegister.toString());
+                            break;
+                        case 0x2:
+                            // Print 0x00 terminating string starting from address in Y register
+                            let currentAddress = this.yRegister;
+                            while (this.MMU.readImmediate(currentAddress) != 0x00) {
+                                let toPrint = Ascii.lookup(this.MMU.readImmediate(currentAddress));
+                                _StdOut.putText(toPrint);
+                                currentAddress++;
+                            }
+                            break;
+                        case 0x3:
+                            // Print 0x00 terminating string starting from address in MMU
+                            let lowString = this.MMU.getLowOrderByte().toString(16);
+                            let highString = this.MMU.getHighOrderByte().toString(16);
+                            let fullString = "0x".concat(highString.concat(lowString));
+                            let desiredAddress = Number(fullString);
+                            while (this.MMU.readImmediate(desiredAddress) != 0x00) {
+                                let toPrint = Ascii.lookup(this.MMU.readImmediate(desiredAddress));
+                                _StdOut.putText(toPrint);
+                                desiredAddress++;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            this.currentStep += 3;
+        }
+
+        private execute2() {
+            // Perform second execute phase if needed
+            // Currently only exists for EE
+            this.accumulator++;
+            this.currentStep++;
+        }
+
+        private writeBack() {
+            // An operation which requires updating a value will need to call writeBack
+            // in order to write the new value back to the memory address
+            // Currently only exists for EE
+            this.MMU.write(this.accumulator);
+            this.currentStep++;
+        }
+
+        private interruptCheck() {
+            // Check for interrupts - if present, put program and register data on stack and execute
+            // interrupt
+            this.IC.emptyNextInterruptQueues();
+            this.currentStep = 0;
+        }
+
     }
 }
