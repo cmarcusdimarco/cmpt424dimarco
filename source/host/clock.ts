27c02b2fe7471d824a2fef6c99f525ddbe08d4f0
C. Marcus DiMarco <79232853+cmarcusdimarco@users.noreply.github.com>
2022-09-29 11:41:13 -0400

    Added clock from CMPT422.

---
+++ source/host/clock.ts
@@ -0,0 +1,24 @@
+import { Hardware } from "./Hardware";
+import { ClockListener } from "./imp/ClockListener";
+
+export class Clock extends Hardware {
+    // Array of ClockListeners
+    private listeners : ClockListener[] = [];
+
+    // Constructor
+    constructor() {
+        super('CLK', 0);
+    }
+
+    // Add new listener to array
+    public addListener(listener: ClockListener) {
+        this.listeners.push(listener);
+    }
+
+    public pulse() {
+        this.log('Clock Pulse Initialized');
+        for (let i = 0; i < this.listeners.length; i++) {
+            this.listeners[i].pulse();
+        }
+    }
+}
