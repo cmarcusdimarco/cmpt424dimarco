28a616960899879440b2002eba0141085facd007
cmarcusdimarco <C.DiMarco1@marist.edu>
2022-09-30 10:05:02 -0400

    Added to TSOS namespace.

--- source/host/clock.ts
+++ source/host/clock.ts
@@ -1,24 +1,24 @@
-import { Hardware } from "./Hardware";
-import { ClockListener } from "./imp/ClockListener";
+module TSOS {
 
-export class Clock extends Hardware {
-    // Array of ClockListeners
-    private listeners : ClockListener[] = [];
+    export class Clock extends Hardware {
+        // Array of ClockListeners
+        private listeners: ClockListener[] = [];
 
-    // Constructor
-    constructor() {
-        super('CLK', 0);
-    }
+        // Constructor
+        constructor() {
+            super('CLK', 0);
+        }
 
-    // Add new listener to array
-    public addListener(listener: ClockListener) {
-        this.listeners.push(listener);
-    }
+        // Add new listener to array
+        public addListener(listener: ClockListener) {
+            this.listeners.push(listener);
+        }
 
-    public pulse() {
-        this.log('Clock Pulse Initialized');
-        for (let i = 0; i < this.listeners.length; i++) {
-            this.listeners[i].pulse();
+        public pulse() {
+            this.log('Clock Pulse Initialized');
+            for (let i = 0; i < this.listeners.length; i++) {
+                this.listeners[i].pulse();
+            }
         }
     }
 }
