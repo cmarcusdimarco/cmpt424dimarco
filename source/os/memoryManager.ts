458ba69b799a0297c761f6c3cd1611e4285a9403
C. Marcus DiMarco <79232853+cmarcusdimarco@users.noreply.github.com>
2022-10-03 12:37:35 -0400

    #9: Outlined MemoryManager for allocation of memory for user programs.

---
+++ source/os/memoryManager.ts
@@ -0,0 +1,21 @@
+/**
+ * The Memory Manager is responsible for allocating and deallocating memory to the operating system.
+ * Therefore, it must interface with the memory modules via system calls.
+ */
+
+module TSOS {
+    export class MemoryManager {
+        public baseRegister: number = 0x0000;               // First physical address to allocate
+        public readonly limitRegister: number = 0x0100;     // Maximum range of logical addresses
+
+        public allocateMemory(program: number[]) {
+            // Query current baseRegister for existing data
+            if (_MemoryAccessor.readImmediate(this.baseRegister) != 0x0000) {
+                _StdOut.putText('ERR: Unable to allocate memory - memory block already in use.');
+                return;
+            }
+
+            // Allocate memory
+        }
+    }
+}
