373e84f3b9635951ea0774dcf7576d86e596e928
C. Marcus DiMarco <79232853+cmarcusdimarco@users.noreply.github.com>
2022-10-27 10:54:51 -0400

    #17: Added multiple loads to memoryManager - need more memory elements in HTML.

--- source/os/memoryManager.ts
+++ source/os/memoryManager.ts
@@ -15,32 +15,44 @@ module TSOS {
 
         // Allocates memory for the program passed via param.
         public allocateMemory(program: number[]) {
-            // Query current baseRegister for existing data
-            if (_MemoryAccessor.readImmediate(this.baseRegister) != 0x0000) {
-                _StdOut.putText('ERR: Unable to allocate memory - memory block already in use.');
-                return;
+
+            // Query memory partitions for existing data
+            for (let partitionBaseAddress of _Memory.partitions) {
+                if (_MemoryAccessor.readImmediate(partitionBaseAddress) == 0x0000) {
+
+                    // Validate program length - return error if overflow
+                    if (program.length >= this.limitRegister) {
+                        _StdOut.putText('ERR: Unable to allocate memory - user program exceeds available memory.');
+                        return;
+                    }
+
+                    // Allocate memory, calling the Memory Accessor to write the program to the allocated space.
+                    this.baseRegister = partitionBaseAddress;
+                    _MemoryAccessor.writeProgram(program, this.baseRegister, this.limitRegister);
+
+                    // Create the Process Control Block, assign a process ID, and push to the registered processes array.
+                    let processControlBlock = new ProcessControlBlock(this.processIdCounter++, this.baseRegister, _CPU);
+                    this.registeredProcesses.push(processControlBlock);
+
+                    // Write the Process Control Block to index.html to test this functionality
+                    // TODO: Clean this up for a proper display of stored processes.
+                    let textArea = document.getElementById('taProcessControlBlock');
+                    if (!textArea.textContent) {
+                        textArea.textContent = `PID: ${processControlBlock.processId} State: ${processControlBlock.state}\r\n`;
+                    } else {
+                        // TODO: Does not yet update input if textArea has text already in it.
+                        textArea.append(`PID: ${processControlBlock.processId} State: ${processControlBlock.state}\r\n`);
+                    }
+
+                    // Print info to console
+                    _StdOut.putText(`Program loaded into memory block ${this.baseRegister / this.limitRegister} ` +
+                        `with process ID ${processControlBlock.processId}.`);
+                    return;
+                }
             }
 
-            // Allocate memory, calling the Memory Accessor to write the program to the allocated space.
-            _MemoryAccessor.writeProgram(program, this.baseRegister, this.limitRegister);
-
-            // Create the Process Control Block, assign a process ID, and push to the registered processes array.
-            let processControlBlock = new ProcessControlBlock(this.processIdCounter++, this.baseRegister, _CPU);
-            this.registeredProcesses.push(processControlBlock);
-
-            // Write the Process Control Block to index.html to test this functionality
-            // TODO: Clean this up for a proper display of stored processes.
-            let textArea = document.getElementById('taProcessControlBlock');
-            if (!textArea.textContent) {
-                textArea.textContent = `PID: ${processControlBlock.processId} State: ${processControlBlock.state}\r\n`;
-            } else {
-                // TODO: Does not yet update input if textArea has text already in it.
-                textArea.append(`PID: ${processControlBlock.processId} State: ${processControlBlock.state}\r\n`);
-            }
-
-            // Print info to console
-            _StdOut.putText(`Program loaded into memory block ${this.baseRegister / this.limitRegister} ` +
-                                 `with process ID ${processControlBlock.processId}.`);
+            // Print error message if memory is full
+            _StdOut.putText('ERR: Unable to allocate memory - all memory partitions already in use.');
         }
 
         // Deallocates the memory assigned to a process after execution.
