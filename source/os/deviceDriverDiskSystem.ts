1096023b673108cc6ba1f8993482fa62aa813423
C. Marcus DiMarco <79232853+cmarcusdimarco@users.noreply.github.com>
2022-12-08 13:54:08 -0500

    #22: Added format command to driver using sessionStorage.

--- source/os/deviceDriverDiskSystem.ts
+++ source/os/deviceDriverDiskSystem.ts
@@ -19,6 +19,28 @@ module TSOS {
         }
 
         // Format
+        public format() {
+
+            // These variable is redundant, but they help with readability to reference the long strings as the shorter
+            // variable names for the loops below. The strings are space-delineated to partition between the
+            // active flag, the reference pointer to the next storage address, and the actual data within
+            let zeroString = "0 000 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+            let masterBootRecordString = "1 000 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+
+            // Initialize and/or reset the storage map
+            for (let track = 0; track < 4; track++) {
+                for (let sector = 0; sector < 8; sector++) {
+                    for (let block = 0; block < 8; block++) {
+                        // Skip the master boot record, held at 0:0:0
+                        if (track + sector + block === 0) {
+                            sessionStorage.setItem(`${track}:${sector}:${block}`, masterBootRecordString);
+                        }
+                        sessionStorage.set(`${track}:${sector}:${block}`, zeroString);
+                    }
+                }
+            }
+
+        }
 
         // Create filename
 
